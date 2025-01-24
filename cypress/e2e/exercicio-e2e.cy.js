5ae609c065f854730b2c6a2a78d9dc72d87a02e6
Renan Ferreira <renan.ferrer@hotmail.com>
2025-01-24 20:33:51 -0300

    Faz um pedido na loja Ebac Shop de ponta a ponta

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -15,19 +15,63 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-    produtosPage.visitarProduto('Aether Gym Pant')
-    cy.get('#tab-title-description > a').should('contain', "Descrição")
+    //Adiciona o primeiro produto no carrinho
+    cy.fixture('produtos').then(dados => {
+      produtosPage.visitarProduto(dados[0].nomeProduto)
+      produtosPage.addProdutoCarrinho(
+        dados[0].tamanho, 
+        dados[0].cor, 
+        dados[0].quantidade)
+      cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+    })
 
-    produtosPage.visitarProduto('Ajax Full-Zip Sweatshirt')
-    cy.get('#tab-title-description > a').should('contain', "Descrição")
+    //Adiciona o segundo produto no carrinho
+    cy.fixture('produtos').then(dados => {
+      produtosPage.visitarProduto(dados[1].nomeProduto)
+      produtosPage.addProdutoCarrinho(
+        dados[1].tamanho, 
+        dados[1].cor, 
+        dados[1].quantidade)
+      cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+    })
 
-    produtosPage.visitarProduto('Ariel Roll Sleeve Sweatshirt')
-    cy.get('#tab-title-description > a').should('contain', "Descrição")
+    //Adiciona o terceiro produto no carrinho
+    cy.fixture('produtos').then(dados => {
+      produtosPage.visitarProduto(dados[2].nomeProduto)
+      produtosPage.addProdutoCarrinho(
+        dados[2].tamanho, 
+        dados[2].cor, 
+        dados[2].quantidade)
+      cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+    })
 
-    produtosPage.visitarProduto('Atlas Fitness Tank')
-    cy.get('#tab-title-description > a').should('contain', "Descrição")    
-      
+    //Adiciona o quarto produto no carrinho
+    cy.fixture('produtos').then(dados => {
+      produtosPage.visitarProduto(dados[3].nomeProduto)
+      produtosPage.addProdutoCarrinho(
+        dados[3].tamanho, 
+        dados[3].cor, 
+        dados[3].quantidade)
+      cy.get('.woocommerce-message').should('contain', dados[3].nomeProduto)
+    })
+
+    //Checkout
+    cy.get('.woocommerce-message > .button').click()
+    cy.get('.checkout-button').click()
+    cy.get('.showlogin').click()
+
+    //Login no checkout
+    cy.fixture('perfil').then(dadosLogin => {
+      cy.get('#username').type(dadosLogin.usuario)
+      cy.get('#password').type(dadosLogin.senha)
+      cy.get('.woocommerce-button').click()
+    })
+
+    //Finalização
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+    cy.get('.page-title').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
   });
-
 
 })
--- cypress/support/page_objects/produtos.page.js
+++ cypress/support/page_objects/produtos.page.js
@@ -20,8 +20,11 @@ class ProdutosPage{
         cy.visit(`produtos/${urlFormatada}`)
     }
 
-    addProdutoCarrinho() {
-
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get(`.button-variable-item-${tamanho}`).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
     }
 
 }
